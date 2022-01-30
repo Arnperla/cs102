52edcf54689b25199d4a2bc1e2d31b1e2e892866
Arnperla <97864005+Arnperla@users.noreply.github.com>
2022-01-30 18:03:14 -0500

    Add files via upload

---
+++ timerloop.js
@@ -0,0 +1,28 @@
+//Arnold Perla
+
+// used to create a function named countdown to operate the count down timer
+function Countdown() {
+    // variable used to set count down timer to 10
+    var currtime = 10
+    // for loop where if the timer is = to 11 it prints blastoff
+    for (var i = 1; i <=11; i++) {
+        if (i == 11) {
+            setTimeout(function () {
+                document.getElementById("countdowntimer").innerHTML = "Blastoff!";
+            }, 1000 * i);
+            // used to display warning message when timer is halfway done
+        } else if (i > 6){
+            setTimeout(function () {
+                document.getElementById("countdowntimer").innerHTML =
+                "Warning! Halfway to launch, time left =" + currtime;
+                currtime = currtime - 1;
+            }, 1000 * i);
+            // used to display current time
+        }else {
+            setTimeout(function () {
+              document.getElementById("countdowntimer").innerHTML = "the time left is" + currtime;  
+              currtime = currtime - 1;
+            }, 1000 * i);
+        }
+    }
+}
